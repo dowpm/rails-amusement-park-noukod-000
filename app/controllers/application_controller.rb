384885d8874a0a8f7af060fa39ac5ffd1c03118f
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-19 16:05:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   before_action :redirect_if_not, except: [:home]
   before_action :is_loging?, only: :home
 
+  unless Rails.application.config.consider_all_requests_local
+   rescue_from ActionController::RoutingError, with: -> { render_404  }
+ end
+
+ def render_404
+   respond_to do |format|
+     format.html { render template: 'errors/not_found', status: 404 }
+     format.all { render nothing: true, status: 404 }
+   end
+ end
+
   def home
     # binding.pry
   end
